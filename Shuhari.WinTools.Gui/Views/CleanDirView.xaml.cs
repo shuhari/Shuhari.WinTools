7a27345efc8b305f6dd578fbc3e2b338e8aead56
yuhao <1302415556@qq.com>
2016-06-14 08:52:03 +0800

    Migrate CleanDir feature

--- Shuhari.WinTools.Gui/Views/CleanDirView.xaml.cs
+++ Shuhari.WinTools.Gui/Views/CleanDirView.xaml.cs
@@ -1,4 +1,12 @@
-﻿namespace Shuhari.WinTools.Gui.Views
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using Shuhari.Library.Wpf;
+
+namespace Shuhari.WinTools.Gui.Views
 {
     /// <summary>
     /// Interaction logic for CleanDirView.xaml
@@ -8,6 +16,145 @@
         public CleanDirView()
         {
             InitializeComponent();
+
+            Loaded += CleanDirView_Loaded;
+        }
+
+        private BackgroundWorker _worker;
+        private ObservableCollection<string> _logs;
+
+        private void CleanDirView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _worker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true,
+            };
+            _worker.DoWork += worker_DoWork;
+            _worker.ProgressChanged += worker_ProgressChanged;
+            _worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+
+            _logs = new ObservableCollection<string>();
+            logList.ItemsSource = _logs;
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var path = this.BrowseForFolder();
+            if (path != null)
+            {
+                txtDirName.Text = path;
+            }
+        }
+
+        private void btnClean_Click(object sender, RoutedEventArgs e)
+        {
+            string baseDir = txtDirName.Text;
+            if (!string.IsNullOrWhiteSpace(baseDir))
+            {
+                btnClean.IsEnabled = false;
+                _logs.Clear();
+                _worker.RunWorkerAsync(baseDir);
+            }
+        }
+
+        void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnClean.IsEnabled = true;
+            _logs.Add("清理完毕");
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            var log = e.UserState as string;
+            if (log != null)
+            {
+                _logs.Add(log);
+                logList.SelectedItem = log;
+                logList.ScrollIntoView(log);
+            }
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string baseDir = (string)e.Argument;
+
+            CleanDir(new DirectoryInfo(baseDir));
+        }
+
+        private DirectoryInfo[] SafeGetDirectories(DirectoryInfo di)
+        {
+            try
+            {
+                return di.GetDirectories();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return new DirectoryInfo[0];
+            }
+        }
+
+        private FileInfo[] SafeGetFiles(DirectoryInfo di)
+        {
+            try
+            {
+                return di.GetFiles();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return new FileInfo[0];
+            }
+        }
+
+        private void CleanDir(DirectoryInfo di)
+        {
+            foreach (var subDi in SafeGetDirectories(di))
+            {
+                CleanDir(subDi);
+            }
+
+            var filesToDelete = SafeGetFiles(di)
+                .Where(IsDeletableFile)
+                .ToArray();
+            foreach (var fi in filesToDelete)
+            {
+                try
+                {
+                    fi.Delete();
+                }
+                catch (Exception exp)
+                {
+                    var log = string.Format("删除文件失败: {0}, 原因={1}", fi.FullName, exp.Message);
+                    _worker.ReportProgress(0, log);
+                }
+            }
+
+            if (SafeGetDirectories(di).Length == 0 && SafeGetFiles(di).Length == 0)
+            {
+                try
+                {
+                    di.Delete();
+                    var log = string.Format("删除目录成功: {0}", di.FullName);
+                    _worker.ReportProgress(0, log);
+                }
+                catch (Exception exp)
+                {
+                    var log = string.Format("删除目录失败: {0}, 原因={1}", di.FullName, exp.Message);
+                    _worker.ReportProgress(0, log);
+                }
+            }
+        }
+
+        private bool IsDeletableFile(FileInfo fi)
+        {
+            string[] names = { "Thumbs.db" };
+            foreach (var name in names)
+            {
+                if (fi.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
