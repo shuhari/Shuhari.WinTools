d0719fb6258a55051f537cf8a5afd5944b9fb13c
yuhao <1302415556@qq.com>
2016-06-14 09:06:10 +0800

    Migrate Service Manage feature

---
+++ Shuhari.WinTools.Core/Features/ServiceManage/ServiceItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace Shuhari.WinTools.Core.Features.ServiceManage
+{
+    public class ServiceItem
+    {
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Description { get; set; }
+
+        public ServiceControllerStatus Status { get; set; }
+
+        public ServiceStartMode StartMode { get; set; }
+    }
+}
--- Shuhari.WinTools.Gui/Views/ServiceManageView.xaml.cs
+++ Shuhari.WinTools.Gui/Views/ServiceManageView.xaml.cs
@@ -1,4 +1,11 @@
-﻿namespace Shuhari.WinTools.Gui.Views
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Management;
+using System.ServiceProcess;
+using System.Windows;
+using Shuhari.WinTools.Core.Features.ServiceManage;
+
+namespace Shuhari.WinTools.Gui.Views
 {
     /// <summary>
     /// Interaction logic for ServiceManageView.xaml
@@ -8,6 +15,52 @@
         public ServiceManageView()
         {
             InitializeComponent();
+
+            Loaded += ServiceManageView_Loaded;
+        }
+
+        private ObservableCollection<ServiceItem> _services;
+
+        private void ServiceManageView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _services = new ObservableCollection<ServiceItem>();
+            list.ItemsSource = _services;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReloadServices();
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            ReloadServices();
+        }
+
+        private void ReloadServices()
+        {
+            _services.Clear();
+
+            var services = ServiceController.GetServices();
+            foreach (var service in services)
+            {
+                var si = new ServiceItem();
+                si.Name = service.ServiceName;
+                si.DisplayName = service.DisplayName;
+                si.Status = service.Status;
+                string objPath = string.Format("Win32_Service.Name='{0}'", si.Name);
+                using (ManagementObject mo = new ManagementObject(new ManagementPath(objPath)))
+                {
+                    si.Description = Convert.ToString(mo["Description"]);
+                    var sm = (string)mo["StartMode"];
+                    ServiceStartMode startMode = ServiceStartMode.Automatic;
+                    Enum.TryParse<ServiceStartMode>(sm, out startMode);
+                    si.StartMode = startMode;
+                }
+
+                _services.Add(si);
+                service.Dispose();
+            }
         }
     }
 }
